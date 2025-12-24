709277c3b6b51317a00af6c9052ebf9562d6e778
surya <suryabhanupandeypandey@gmail.com>
2025-12-24 16:32:15 +0530

    Operator & DataType

---
+++ Basic/Operator.java
@@ -0,0 +1,19 @@
+package Com.app;
+
+public class Operator
+{
+	public static void main(String args[])
+		{
+		int a =12;
+		int b = 20;
+		int c = a+b;
+		
+		System.out.println("Sum :: "+c +" , Multiple :: " +(a*b)+ ", Divide :: "+(b/a)+ ", Remainder ::  " +(a%b));
+
+
+
+		}
+
+
+
+}
---
+++ Basic/StudentInfo.java
@@ -0,0 +1,25 @@
+package Com.app;
+
+
+public class StudentInfo
+{
+	public static void main(String args[])
+		{
+			String Name ="Surya Bhanu Pandey";
+			int age = 21;
+			char grade = 'A';
+			boolean isPass = true;
+			double marks = 90.5;
+
+
+			System.out.println("Name : "+Name);
+			System.out.println("Age : "+age);
+			System.out.println("Grade : "+grade );
+			System.out.println("Pass : "+isPass);
+			System.out.println("Marks : "+marks);
+
+		} 
+
+
+
+}
